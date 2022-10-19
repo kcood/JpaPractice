f3aa6ffde1383debbb3cf012beb02e48c5ffb9b4
DYS <kcood92@gmail.com>
2022-10-19 14:37:10 +0900

    feat: WebView - 홈 화면과 레이아웃

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,8 +3,9 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -12,12 +13,16 @@ public class OrderRepository {
 
     private final EntityManager em;
 
-    public void save(Order order){
+    public void save(Order order) {
         em.persist(order);
     }
 
-    public Order findOne(Long id){
+    public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//    }
 
 }
